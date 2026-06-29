2d2edc788f0d1be23ce626f8eb29b717bedba103
kimoade <130127406+kimoade@users.noreply.github.com>
2026-06-28 23:38:48 -0700

    Implement all functions in src/converter.cpp according to converter.hpp

--- src/converter.cpp
+++ src/converter.cpp
@@ -1,19 +1,49 @@
 #include "converter.hpp"
+#include <iostream>
+
+using namespace std;
+
+void printMenu() {
+    cout << "\nUnit Converter" << endl << endl;
+    cout << "1. Inches to centimeters" << endl;
+    cout << "2. Centimeters to inches" << endl;
+    cout << "3. Pounds to kilograms" << endl;
+    cout << "4. Kilograms to pounds" << endl;
+    cout << "5. Fahrenheit to Celsius" << endl;
+    cout << "6. Celsius to Fahrenheit" << endl;
+    cout << "0. Exit" << endl << endl;
+    cout << "Enter choice: ";
+}
+
+bool isValidMenuChoice(int choice) {
+    return (choice >= 1 && choice <= 6) || choice == EXIT_CHOICE;
+}
+
+bool requiresNonNegativeValue(int choice) {
+    return (choice >= 1 && choice <= 4);
+}
+
+bool isValidValueForChoice(int choice, double value) {
+    if (requiresNonNegativeValue(choice)) {
+        return value >= 0;
+    }
+    return true;
+}
 
 double inchesToCentimeters(double inches) {
-    return inches * 2.54;
+    return inches * CENTIMETERS_PER_INCH;
 }
 
 double centimetersToInches(double centimeters) {
-    return centimeters / 2.54;
+    return centimeters / CENTIMETERS_PER_INCH;
 }
 
 double poundsToKilograms(double pounds) {
-    return pounds * 0.453592;
+    return pounds / POUNDS_PER_KILOGRAM;
 }
 
 double kilogramsToPounds(double kilograms) {
-    return kilograms / 0.453592;
+    return kilograms * POUNDS_PER_KILOGRAM;
 }
 
 double fahrenheitToCelsius(double fahrenheit) {
